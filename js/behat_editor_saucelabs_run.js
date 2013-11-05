b17c27835dbd579cf06c73b5cfd29ccab63f208d
alnutile <alfrednutile@gmail.com>
2013-11-05 16:15:28 -0500

    minor fixes

--- js/behat_editor_saucelabs_run.js
+++ js/behat_editor_saucelabs_run.js
@@ -9,47 +9,43 @@
                 url: '/admin/behat/saucelabs/jobs',
                 success: function(data) { Drupal.behat_editor_saucelabs.getStatus(data, tries, max_tries, starting_job_id); }
             }
-        )};
+        );
+    };
 
     Drupal.behat_editor_saucelabs.getStatus = function(data, tries, max_tries, starting_job_id) {
-            if(starting_job_id == 0) {
-                starting_job_id = data.latest_id;
-                Drupal.behat_editor.renderMessageCustom('Connecting to Saucelabs and waiting for job feedback try '+ tries + ' of ' + max_tries, 'info');
-                tries++;
-                Drupal.behat_editor_saucelabs.saucelabs_check(tries, starting_job_id);
-            } else if (tries < max_tries && starting_job_id === data.latest_id) {
-                //Drupal.behat_editor.renderMessageCustom('Connecting to Saucelabs and waiting for job feedback try '+ tries + ' of ' + max_tries, 'info');
-                if(tries === 1) {
-                    if($('div.sl-running').length) {
-                        $('div.sl-running').fadeOut().remove();
-                    }
-                    var message = "<div class='alert alert-info sl-running'>";
-                    message += '<div>Connecting to Saucelabs and waiting for job feedback try <span class="tries">'+ tries + '</span> of ' + max_tries + '</div>';
-                    message += '</div>';
-                    $('#messages-behat').append(message);
-                } else {
-                    $('div.sl-running span.tries').text(tries);
+        if(starting_job_id == 0) {
+            starting_job_id = data.latest_id;
+            Drupal.behat_editor.renderMessageCustom('Connecting to Saucelabs and waiting for job feedback try '+ tries + ' of ' + max_tries, 'info');
+            tries++;
+            Drupal.behat_editor_saucelabs.saucelabs_check(tries, starting_job_id);
+        } else if (tries < max_tries && starting_job_id === data.latest_id) {
+            if(tries === 1) {
+                if($('div.sl-running').length) {
+                    $('div.sl-running').fadeOut().remove();
                 }
-                tries++;
-                Drupal.behat_editor_saucelabs.saucelabs_check(tries, starting_job_id);
+                var message = "<div class='alert alert-info sl-running'>";
+                message += '<div>Connecting to Saucelabs and waiting for job feedback try <span class="tries">'+ tries + '</span> of ' + max_tries + '</div>';
+                message += '</div>';
+                $('#messages-behat').append(message);
             } else {
-                //See if we are done cause of max count or because we have a repsonse
-                if(starting_job_id != data.latest_id) {
-                    var id = data.latest_id;
-                    //var url = data.latest_job.video_url;
-                    var url = '<a href="https://saucelabs.com/tests/'+ id + '" target="_blank" class="btn btn-success">Job is here</a>';
-                    //var screenshot = 'https://saucelabs.com/jobs/'+id+'/0000screenshot.png';
-                    var status = data.latest_job.status;
-                    Drupal.behat_editor.renderMessageCustom('New SauceLabs job info id ' +id+ ' @ ' +url+ '. Status of the job is "' +status+'"');
-                    Drupal.behat_editor_saucelabs.getJobInfo(id, 0);
-                    return id;
-                } else {
-                    Drupal.behat_editor.renderMessageCustom('We have reached the mx tries  '+ tries + ' of ' + max_tries + '<br>' +
-                        'SauceLabs has not responded with a new ID. That does not mean the test did not work though. ' +
-                        'Please review your account <a href="https://saucelabs.com/account" target="_blank">Dashboard</a>' +
-                        ' for SauceLabs.', 'info');
-                }
+                $('div.sl-running span.tries').text(tries);
             }
+            tries++;
+            Drupal.behat_editor_saucelabs.saucelabs_check(tries, starting_job_id);
+        } else {
+            if(starting_job_id != data.latest_id) {
+                var id = data.latest_id;
+                var url = '<a href="https://saucelabs.com/tests/'+ id + '" target="_blank" class="btn btn-success">Job is here</a>';
+                var status = data.latest_job.status;
+                Drupal.behat_editor.renderMessageCustom('New SauceLabs job info id ' +id+ ' @ ' +url+ '. Status of the job is "' +status+'"');
+                Drupal.behat_editor_saucelabs.getJobInfo(id, 0);
+            } else {
+                Drupal.behat_editor.renderMessageCustom('We have reached the mx tries  '+ tries + ' of ' + max_tries + '<br>' +
+                    'SauceLabs has not responded with a new ID. That does not mean the test did not work though. ' +
+                    'Please review your account <a href="https://saucelabs.com/account" target="_blank">Dashboard</a>' +
+                    ' for SauceLabs.', 'info');
+            }
+        }
     };
 
     Drupal.behat_editor_saucelabs.getJobInfo = function(job_id, run) {
@@ -87,7 +83,6 @@
     }
 
     Drupal.behaviors.behat_editor_saucelabs_run = {
-
         attach: function (context) {
             $('a.sauce').click(function(e){
                 //Drupal.behat_editor.buttons('disable');
@@ -108,8 +103,6 @@
                     var urlFilename = url + filename;
                     var latestId = '';
                     var data = {};
-                    var newId = '';
-                    //Add this first to get previous job id
                     var getID = $.getJSON('/admin/behat/saucelabs/jobs', function(data){
                         latestId = data.latest_id;
                         Drupal.behat_editor_saucelabs.saucelabs_check(1, latestId);
